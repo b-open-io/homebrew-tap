8f3829da9ab82f1f0c99d3130acf678cca780313
Satchmo <rohenaz@gmail.com>
2025-11-30 10:20:54 -0500

    Update scribe cask to use b-open-io/scribe releases

--- Casks/scribe.rb
+++ Casks/scribe.rb
@@ -2,7 +2,7 @@ cask "scribe" do
   version "0.0.1"
   sha256 :no_check # Update with actual SHA256 once DMG is built
 
-  url "https://github.com/b-open-io/scribe-desktop/releases/download/v#{version}/Scribe-#{version}.dmg"
+  url "https://github.com/b-open-io/scribe/releases/download/v#{version}/Scribe-#{version}.dmg"
   name "Scribe"
   desc "Timeline-based work evidence aggregator"
   homepage "https://scribe.cool"
