70e22da97015987f154aeb679885eb76c3da9089
CircleCI <ci@bopen.io>
2026-02-20 00:53:36 +0000

    Update Scribe to 0.0.62

--- Casks/scribe.rb
+++ Casks/scribe.rb
@@ -1,6 +1,6 @@
 cask "scribe" do
-  version "0.0.60"
-  sha256 "6d4fad84e4784379aa119f62ada8d1241cc228fed7e09cd0f5e287a9970a4ed1"
+  version "0.0.62"
+  sha256 "e82efb22cb51222600d4d1b432d605a3c19ec69a808ad120912cb9f4ccd848ab"
 
   url "https://jkygjvqfie32bnvq.public.blob.vercel-storage.com/releases/Scribe-#{version}.dmg"
   name "Scribe"
